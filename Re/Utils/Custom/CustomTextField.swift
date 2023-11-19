0a6c4694bfa9f610294617723fafa0e53b04c853
한 <hih458@naver.com>
2023-11-19 21:03:04 +0900

    [Add] CustomTextField 생성
    - HoshiTextField 적용

---
+++ Re/Utils/Custom/CustomTextField.swift
@@ -0,0 +1,52 @@
+//
+//  CustomTextField.swift
+//  Re
+//
+//  Created by 황인호 on 11/19/23.
+//
+
+import UIKit
+import TextFieldEffects
+
+class CustomTextField: HoshiTextField {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setTextField()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setTextField() {
+        placeholderColor = .lightGray
+        borderActiveColor = .systemCyan
+        borderInactiveColor = .lightGray
+        
+        returnKeyType = .next
+    }
+    
+    
+}
+
+class CustomLastTextField: HoshiTextField {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setTextField()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setTextField() {
+        placeholderColor = .lightGray
+        borderActiveColor = .systemCyan
+        borderInactiveColor = .lightGray
+        
+        returnKeyType = .done
+    }
+}
+
+
