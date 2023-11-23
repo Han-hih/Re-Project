650e5b8ccbc91db01e689e22c54955dff0741499
한 <hih458@naver.com>
2023-11-23 16:38:07 +0900

    [Feat] 이메일 중복 확인 기능 구현

--- Re/ViewModels/RegisterViewModel.swift
+++ Re/ViewModels/RegisterViewModel.swift
@@ -7,22 +7,29 @@
 
 import Foundation
 import RxSwift
+import Moya
 
 final class RegisterViewModel: ViewModelType {
+    
+    private let service = MoyaProvider<APIManager>()
     
     private let disposeBag = DisposeBag()
     
     struct Input {
         let emailValid: Observable<String>
         let passwordValid: Observable<String>
+        let emailDuplicateTap: Observable<Void>
     }
     
     struct Output {
         let emailValid: Observable<Bool>
         let passwordValid: Observable<Bool>
+        let emailDuplicateTap: PublishSubject<EmailValidResult>
     }
     
     func transform(input: Input) -> Output {
+        
+        let emailDuplicateTap = PublishSubject<EmailValidResult>()
         
         let emailValid = input.emailValid.map { ValidationCheck().isValidEmail($0)
         }
@@ -30,7 +37,41 @@ final class RegisterViewModel: ViewModelType {
         let passwordValid = input.passwordValid.map { ValidationCheck().validatePassword($0)
         }
         
-        return Output(emailValid: emailValid, passwordValid: passwordValid)
+        input.emailDuplicateTap
+            .withLatestFrom(input.emailValid) { _, query in
+                return query
+            }
+            .flatMap { self.checkEmailDuplicate(email: $0) }
+            .subscribe(with: self) { owner, value in
+                print(value)
+                emailDuplicateTap.onNext(value)
+            }
+            .disposed(by: disposeBag)
+        
+        return Output(emailValid: emailValid,
+                      passwordValid: passwordValid,
+                      emailDuplicateTap: emailDuplicateTap
+        )
     }
     
+    private func checkEmailDuplicate(email: String) -> Single<EmailValidResult> {
+        return Single<EmailValidResult>.create { single in
+            self.service.request(APIManager.emailValid(email: email)) { result in
+                switch result {
+                case .success(let element):
+                    guard let data = try? element.map(EmailValidResult.self) else { return }
+                    single(.success(data))
+                case .failure(let error):
+                    print(error)
+                }
+            }
+            return Disposables.create()
+        }
+    }
+    
+    
+    
 }
+
+
+
