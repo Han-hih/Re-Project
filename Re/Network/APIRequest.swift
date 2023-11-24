47d1468bf4443f34f1fd81defafafa3168390062
한 <hih458@naver.com>
2023-11-24 13:11:30 +0900

    [Feat] 네트워크 에러코드 대응

--- Re/Network/APIRequest.swift
+++ Re/Network/APIRequest.swift
@@ -8,26 +8,44 @@
 import Foundation
 import RxSwift
 import Moya
-    
+
+enum NetworkError: Int, Error {
+    case valueEmpty = 400
+    case invalidEmail = 409
+    case decodingFailed = 999
+    case unownedError = 1
+}
+
 final class APIRequest {
     
     static let shared = APIRequest()
     
-     let service = MoyaProvider<APIManager>()
+    let service = MoyaProvider<APIManager>()
     
-    func checkEmailDuplicate(email: String) -> Single<EmailValidResult> {
-        return Single<EmailValidResult>.create { single in
+    func checkEmailDuplicate(email: String) -> Single<Result<EmailValidResult, NetworkError>> {
+        return Single<Result<EmailValidResult, NetworkError>>.create { single in
             self.service.request(APIManager.emailValid(email: email)) { result in
                 switch result {
-                case .success(let element):
-                    guard let data = try? element.map(EmailValidResult.self) else { return }
-                    single(.success(data))
+                case .success(let response):
+                    print(response.statusCode)
+                    guard let data = try? JSONDecoder().decode(EmailValidResult.self, from: response.data) else
+                    {
+                        single(.success(.failure(.decodingFailed)))
+                        return
+                    }
+                    single(.success(.success(data)))
+
                 case .failure(let error):
-                    print(error)
+                    guard let customError = NetworkError(rawValue: error.response?.statusCode ?? 1) else {
+                        single(.success(.failure(NetworkError.unownedError)))
+                        return
+                    }
+                    single(.success(.failure(customError)))
                 }
             }
             return Disposables.create()
         }
     }
+    
     
 }
