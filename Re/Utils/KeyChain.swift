810d8ffe8372e301e64cbc7c068e07a51a616ade
한 <hih458@naver.com>
2023-11-28 00:43:26 +0900

    [Feat] 키체인 값을 읽어오는 함수 생성

--- Re/Utils/KeyChain.swift
+++ Re/Utils/KeyChain.swift
@@ -13,21 +13,38 @@ final class KeyChain {
     static let shared = KeyChain()
     
     func create(key: String, token: String) {
-        let addQuery: NSDictionary = [
+        let query: NSDictionary = [
             kSecClass: kSecClassGenericPassword,
             kSecAttrAccount: key,
             kSecValueData: token.data(using: .utf8, allowLossyConversion: false) as Any
         ]
-        SecItemDelete(addQuery)
+        SecItemDelete(query)
 //        SecItemAdd(addQuery, nil)
-        let status: OSStatus = SecItemAdd(addQuery, nil)
+        let status: OSStatus = SecItemAdd(query, nil)
         assert(status == noErr, "토큰 값 저장에 실패했습니다.")
         NSLog("status = \(status)")
     }
         
-    
-    
-    
-    
+    func read(key: String) -> String? {
+        let query: NSDictionary = [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrAccount: key,
+            kSecReturnData: kCFBooleanTrue!,
+            kSecMatchLimit: kSecMatchLimitOne
+        ]
+        
+        var dataTypeRef: AnyObject?
+        let status = SecItemCopyMatching(query, &dataTypeRef)
+        
+        if (status == errSecSuccess) {
+            let retrievedData = dataTypeRef as! Data
+            let value = String(data: retrievedData, encoding: String.Encoding.utf8)
+            return value
+        } else {
+            print("failed = \(status)")
+            return  nil
+        }
+        
+    }
     
 }
