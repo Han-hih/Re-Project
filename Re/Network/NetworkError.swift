a6131d18988d6a8f0a62b5dbdc32418d14f20cbb
한 <hih458@naver.com>
2023-12-15 16:40:25 +0900

    [Add] 에러 상태코드에 따라 반환값 추가

--- Re/Network/NetworkError.swift
+++ Re/Network/NetworkError.swift
@@ -9,10 +9,37 @@ import Foundation
 
 enum NetworkError: Int, Error {
     case valueEmpty = 400
+    case invalidAccessToken = 401
     case invalidEmail = 409
     case expireToken = 418
+    case expireAccess = 419
+    case manyRequest = 429
+    case notNormal = 444
     case decodingFailed = 999
-    case unownedError = 1
+    case unownedError = 500
+        
+    var errorMessage: String {
+        switch self {
+        case .valueEmpty:
+            "필수값이 없음"
+        case .invalidAccessToken:
+            "유효하지 않은 액세스 토큰"
+        case .invalidEmail:
+            "유효하지 않은 이메일"
+        case .expireToken:
+            "토큰 만료"
+        case .expireAccess:
+            "액세스 토큰 만료"
+        case .manyRequest:
+            "과호출"
+        case .notNormal:
+            "비정상 접근"
+        case .decodingFailed:
+            "디코딩 실패"
+        case .unownedError:
+            "서버에러 일수도?"
+        }
+    }
 }
 
 enum LoginError: Int, Error {
