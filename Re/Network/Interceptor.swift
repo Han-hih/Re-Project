ae857810ad69186e9ab26a15a61414bb12d9ed10
한 <hih458@naver.com>
2023-12-17 13:49:09 +0900

    [Modify] retry 함수 수정

--- Re/Network/Interceptor.swift
+++ Re/Network/Interceptor.swift
@@ -5,17 +5,16 @@
 //  Created by 황인호 on 11/28/23.
 //
 
-import Foundation
 import Alamofire
 import Moya
 import RxSwift
+import UIKit
 
 final class Interceptor: RequestInterceptor {
     
     static let shared = Interceptor()
-    
-//    private let service = MoyaProvider<APIManager>()
     private let disposeBag = DisposeBag()
+    
     private init() { }
     
     func adapt(_ urlRequest: URLRequest, for session: Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
@@ -30,33 +29,17 @@ final class Interceptor: RequestInterceptor {
         var urlRequest = urlRequest
         urlRequest.setValue(accessToken, forHTTPHeaderField: "Authorization")
         urlRequest.setValue(refreshToken, forHTTPHeaderField: "Refresh")
-        print("adator 적용 \(urlRequest.headers)")
         completion(.success(urlRequest))
     }
     
     func retry(_ request: Request, for session: Session, dueTo error: Error, completion: @escaping (RetryResult) -> Void) {
         print("retry 진입")
-            guard let response = request.task?.response as? HTTPURLResponse, response.statusCode == 419, response.statusCode == 418
+            guard let response = request.task?.response as? HTTPURLResponse, response.statusCode == 419
         else {
             completion(.doNotRetryWithError(error))
             return
         }
-        
-        
-        guard let accessToken = KeyChain.shared.read(key: "access") else { return }
-        guard let refreshToken = KeyChain.shared.read(key: "refresh") else { return }
-        // 토큰 갱신 API 호출
-        APIRequest.shared.refreshToken(token: accessToken, refreshToken: refreshToken)
-            .subscribe(with: self) { owner, result in
-                switch result {
-                case .success(let token):
-                    print("리트라이 성공  \(token)")
-                    completion(.retry)
-                case .failure(let error):
-                    completion(.doNotRetryWithError(error))
-                }
-            }
-            .disposed(by: disposeBag)
+        APIRequest.shared.refreshToken()
     }
 }
     
