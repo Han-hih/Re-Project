e6f3520c4e26de04f87d2f26bd6abffa8b68d1ec
한 <hih458@naver.com>
2023-12-19 22:22:21 +0900

    [Add] 디테일뷰로 넘어갔을 때 보여줄 데이터 가져오는 함수 추가

--- Re/Views/HomeDetail/HomeDetailViewModel.swift
+++ Re/Views/HomeDetail/HomeDetailViewModel.swift
@@ -9,12 +9,26 @@ import Foundation
 
 class HomeDetailViewModel {
     
+    var detail: Datum?
+    
     func LikeButtonTapped(id: String, completion: @escaping (Like) -> Void) {
         APIRequest.shared.apiRequest(APIManager.like(id: id), type: Like.self) { result in
             switch result {
             case .success(let response):
                 print(response.like_status)
                 completion(response)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    func getOnePost(id: String) {
+        APIRequest.shared.apiRequest(APIManager.getOnePost(id: id), type: Datum.self) { result in
+            switch result {
+            case .success(let response):
+                self.detail = response
+                print(response)
             case .failure(let error):
                 print(error)
             }
