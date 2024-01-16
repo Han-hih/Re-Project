87e1e3a9fba794903cd6155fe481395832481bde
한 <hih458@naver.com>
2024-01-16 10:55:36 +0900

    ViewModel에서 받아온 데이터 tableView에 표시

--- Re/Views/Profile/MyProfileViewController.swift
+++ Re/Views/Profile/MyProfileViewController.swift
@@ -10,6 +10,8 @@ import Kingfisher
 
 class MyProfileViewController: BaseViewController {
     
+    private let viewModel = MyProfileViewModel()
+    
     override func setConstraints() {
         super.setConstraints()
         setUI()
@@ -19,12 +21,14 @@ class MyProfileViewController: BaseViewController {
     override func configure() {
         super.configure()
         getProfile()
+        lookMyPost()
     }
     
     private func getProfile() {
         APIRequest.shared.apiRequest(APIManager.getProfile, type: Getprofile.self) { result in
             switch result {
             case .success(let response):
+                print(response)
                 self.nickLabel.text = response.nick
                 self.followerLabel.text = "\(response.followers.count) Followers · \(response.following.count) Following"
                 guard let url = URL(string: APIKey.baseURL + (response.profile ?? "")) else { return }
@@ -35,9 +39,17 @@ class MyProfileViewController: BaseViewController {
         }
     }
     
+    private func lookMyPost() {
+        viewModel.lookUpMyPost(id: KeyChain.shared.read(key: "id") ?? "") {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
     private func setUI() {
         
-        [topView, profileImageView, nickLabel, followerLabel, changeInfoButton].forEach {
+        [topView, profileImageView, nickLabel, followerLabel, changeInfoButton, tableView].forEach {
             view.addSubview($0)
         }
         
@@ -66,6 +78,11 @@ class MyProfileViewController: BaseViewController {
         changeInfoButton.snp.makeConstraints {
             $0.leading.equalTo(nickLabel)
             $0.bottom.equalTo(profileImageView)
+        }
+        
+        tableView.snp.makeConstraints {
+            $0.top.equalTo(topView.snp.bottom).offset(10)
+            $0.bottom.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
         }
     }
     
@@ -103,6 +120,17 @@ class MyProfileViewController: BaseViewController {
         return bt
     }()
     
+    private lazy var tableView = {
+        let view = UITableView()
+        view.register(MyPostTableViewCell.self, forCellReuseIdentifier: MyPostTableViewCell.identifier)
+        view.rowHeight = 100
+        view.separatorStyle = .singleLine
+        view.delegate = self
+        view.dataSource = self
+        view.prefetchDataSource = self
+        return view
+    }()
+    
     @objc
     func changeInfo() {
         let vc = ProfileModifyViewController()
@@ -110,5 +138,40 @@ class MyProfileViewController: BaseViewController {
         vc.profileImage = profileImageView.image
         let nav = UINavigationController(rootViewController: vc)
         present(nav, animated: true)
+    }
+}
+
+extension MyProfileViewController: UITableViewDelegate, UITableViewDataSource, UITableViewDataSourcePrefetching {
+    func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+        for indexPath in indexPaths {
+            if viewModel.getData.count - 1 == indexPath.row {
+                lookMyPost()
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        viewModel.getData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: MyPostTableViewCell.identifier, for: indexPath) as? MyPostTableViewCell else { return UITableViewCell() }
+        
+         let data = viewModel.getData
+        
+        let url = URL(string: APIKey.baseURL + (data[indexPath.row]?.image.first ?? "" + ".jpeg"))
+        
+        cell.titleTextView.text = data[indexPath.row]?.title
+        cell.photoImageView.kf.setImage(with: url, options: [.requestModifier(KFModifier.shared.modifier)])
+        
+        guard let id = KeyChain.shared.read(key: "id") else { return UITableViewCell() }
+        if data[indexPath.row]?.likes.contains(id) == true {
+            cell.likeImage.image = UIImage(systemName: "heart.fill")
+        }
+        
+        cell.timeLabel.text = data[indexPath.row]?.time.toSimpleFormattedString()
+        cell.likeCountLabel.text = "\(data[indexPath.row]?.likes.count ?? 0)"
+        cell.commentCount.text = "\(data[indexPath.row]?.comments.count ?? 0)"
+        return cell
     }
 }
